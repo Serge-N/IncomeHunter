cfa14fda592c5a6970377dc8ae0d3748e0dcd22d
Serge Nalishiwa <51508252+Serge-N@users.noreply.github.com>
2021-03-23 09:40:26 +0200

    Add project files.

---
+++ IncomeHunter/IncomeHunter/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using IncomeHunter.ViewModels;
+using IncomeHunter.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace IncomeHunter
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ IncomeHunter/IncomeHunter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using IncomeHunter.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace IncomeHunter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
